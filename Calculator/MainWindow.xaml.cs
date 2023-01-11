34292fd8709ef5ec23b076bc3d740f60ffec8b1c
Daud Ahmed <Daud Ahmed@DESKTOP-DBCCDTH>
2023-01-11 23:22:58 +0500

    Clear and basic operations implemented

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -16,13 +16,96 @@ using System.Windows.Shapes;
 namespace Calculator
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// floateraction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string Result { get; set; }
+        public string Operator { get; set; } = "";
+
+        public string LastValue { get; set; }
+
+        bool clicked = false;
+        bool consecutiveEqual = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            float num = -1;
+
+            Button tmpButton = e.Source as Button;
+            if(float.TryParse(tmpButton.Content.ToString(), out num))
+            {
+                // Operand
+                if (clicked == false)
+                {
+                    Result = $"{Result}{num.ToString()}";
+                    labelResult.Content = Result;
+                } else
+                {
+                    LastValue = Result;
+                    Result = num.ToString();
+                    labelResult.Content = Result;
+                    clicked = false;   
+                }
+            }
+            else
+            {
+                clicked = true;
+                if (!string.IsNullOrEmpty(Result))
+                {
+                    if (consecutiveEqual == true && tmpButton.Content.ToString() == "=")
+                    {
+                        Result = PerformAction();
+                        labelResult.Content = Result;
+                    }
+                    else if (tmpButton.Content.ToString() == "=")
+                    {
+                        var tmp = Result;
+                        Result = PerformAction();
+                        labelResult.Content = Result;
+                        LastValue = tmp;
+                        consecutiveEqual = true;
+                    }
+                    else
+                    {
+                        Operator = tmpButton.Content.ToString();
+                        consecutiveEqual = false;
+                    }
+                }
+            }
+        }
+
+
+        private string PerformAction()
+        {
+            if(Operator == "+") return (float.Parse(LastValue) + float.Parse(Result)).ToString();
+            if(Operator == "x") return (float.Parse(LastValue) * float.Parse(Result)).ToString();
+            if (consecutiveEqual == false)
+            {
+                if (Operator == "-") return (float.Parse(LastValue) - float.Parse(Result)).ToString();
+                if (Operator == "/") return (float.Parse(LastValue) / float.Parse(Result)).ToString();
+            } else
+            {
+                if (Operator == "-") return (float.Parse(Result) - float.Parse(LastValue)).ToString();
+                if (Operator == "/") return (float.Parse(Result) / float.Parse(LastValue)).ToString();
+            }
+
+            return "";
+        }
+
+        private void buttonClear(object sender, RoutedEventArgs e)
+        {
+            Result = "";
+            LastValue = "";
+            Operator = "";
+            consecutiveEqual= false;
+            clicked= false;
+            labelResult.Content= Result;
         }
     }
 }
